9fc8882f63e6409dbd7b3146f6488c189ddc4caf
Hugo Victor <hugovic.it@gmail.com>
2022-08-24 22:38:01 -0300

    Porta definida e interface do swagger configurado

--- Desafio-Back/Program.cs
+++ Desafio-Back/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.EntityFrameworkCore;
 using NDogs.Data;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,32 @@ builder.Services.AddDbContext<AppDbContext>(); //importanto o contexto
 builder.Services.AddIdentity<IdentityUser,IdentityRole>() //Identity
                 .AddEntityFrameworkStores<AppDbContext>()
                 .AddDefaultTokenProviders();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    //c.SwaggerDoc("v1", new OpenApiInfo { Title = "apiagenda", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme(){
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Header de autorizacao JWT usando o esquema Bearer.\r\n\r\nInforme 'Bearer'[espaco] e o seu token.\r\n\r\nExamplo: \'Bearer 12345abcdef\'",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement{{
+          new OpenApiSecurityScheme{
+             Reference = new OpenApiReference{
+                 Type = ReferenceType.SecurityScheme,
+                 Id = "Bearer"
+             }
+          },
+          new string[] {}
+       }
+    });
+});
 //;
 //===============================
 //JWT
